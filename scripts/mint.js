81e81ebb33f3a1b4e918eb269956e4afc3496037
olgtrkn <142830643+olgtrkn@users.noreply.github.com>
2024-08-06 16:19:11 +0000

    Pending changes exported from your codespace

---
+++ scripts/mint.js
@@ -0,0 +1,37 @@
+const hre = require("hardhat");
+const { encryptDataField, decryptNodeResponse } = require("@swisstronik/utils");
+const sendShieldedTransaction = async (signer, destination, data, value) => {
+  const rpcLink = hre.network.config.url;
+  const [encryptedData] = await encryptDataField(rpcLink, data);
+  return await signer.sendTransaction({
+    from: signer.address,
+    to: destination,
+    data: encryptedData,
+    value,
+  });
+};
+
+async function main() {
+  const contractAddress = "0xd1d8b67578f00D3982d958FE3d7F55C37bE3142b";
+  const [signer] = await hre.ethers.getSigners();
+
+  const contractFactory = await hre.ethers.getContractFactory("TestToken");
+  const contract = contractFactory.attach(contractAddress);
+
+  const functionName = "mint100tokens";
+  const mint100TokensTx = await sendShieldedTransaction(
+    signer,
+    contractAddress,
+    contract.interface.encodeFunctionData(functionName),
+    0
+  );
+
+  await mint100TokensTx.wait();
+
+  console.log("Transaction Receipt: ", mint100TokensTx.hash);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
